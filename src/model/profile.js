129fbc6ae273676d79234aacd22933c0d53d0c1d
Kehinde Adedokun <75108469+hi-heavens@users.noreply.github.com>
2023-07-09 14:35:35 +0100

    created the profile endpoint and its schema

---
+++ src/model/profile.js
@@ -0,0 +1,103 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+// Define the Personal Information Schema
+const personalSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: [true, 'First name is required'],
+  },
+  lastName: {
+    type: String,
+    required: [true, 'Last name is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  phone: {
+    type: String,
+    required: [true, 'Phone number is required'],
+  },
+  linkedIn: {
+    type: String,
+    required: [true, 'LinkedIn URL is required'],
+  },
+  gitHub: {
+    type: String,
+    required: [true, 'GitHub URL is required'],
+  },
+});
+// Define the Education schema
+const educationSchema = new mongoose.Schema({
+  institutionAttended: {
+    type: String,
+    required: true,
+  },
+  degree: {
+    type: String,
+    required: true,
+  },
+  graduationDate: {
+    type: Date,
+    required: true,
+  },
+});
+
+// Define the Experience schema
+const experienceSchema = new mongoose.Schema({
+  companyName: {
+    type: String,
+    required: true,
+  },
+  jobTitle: {
+    type: String,
+    required: true,
+  },
+  employmentDate: {
+    type: Date,
+    required: true,
+  },
+  jobDescription: {
+    type: String,
+    required: true,
+  },
+});
+
+// Define the Project schema
+const projectSchema = new mongoose.Schema({
+  projectName: {
+    type: String,
+    required: true,
+  },
+  projectURL: {
+    type: String,
+    required: true,
+  },
+  projectDescription: {
+    type: String,
+    required: true,
+  },
+});
+// Define the Skill schema
+const skillSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+});
+
+// Define the Profile schema
+const profileSchema = new mongoose.Schema({
+  personal: [personalSchema],
+  educations: [educationSchema],
+  experiences: [experienceSchema],
+  projects: [projectSchema],
+  skills: [skillSchema],
+});
+
+// Create the Profile model
+const Profile = mongoose.model('Profile', profileSchema);
+
+module.exports = Profile;
