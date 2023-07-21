9edf53eb52104a386a65c2813bd7904af4fd1cb0
Kehinde Adedokun <75108469+hi-heavens@users.noreply.github.com>
2023-07-21 10:06:52 +0100

    updated the profile controller to use patch and get and handle requests

--- src/api/profile/profileController.js
+++ src/api/profile/profileController.js
@@ -1,15 +1,39 @@
 const Profile = require('../../model/profileModel');
+const User = require('../../model/user');
 
 async function createProfile(req, res) {
-  const profile = new Profile(req.body);
+  const user = req.body.userId;
+  // const savedProfile = await profile.save();
+  await Profile.updateOne({ userId: user }, req.body, { upsert: true })
+    .then((doc) => {
+      if (doc.upsertedCount === 1) {
+        res.status(200).json({
+          status: "success",
+          message: "profile created successfully"
+        })
+      } else {
+        res.status(200).json({
+          status: "success",
+          message: "profile updated successfully"
+        })
+      }
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ status: 'failed', error: 'Internal Server Error' });
+    });
+}
 
-  const savedProfile = await profile.save();
-  res.status(201).json({
+async function getProfile(req, res) {
+  const loginUser = await Profile.find({ userId: req.body.userId });
+  res.status(200).json({
     status: 'success',
-    message: savedProfile,
+    profile: loginUser,
   });
 }
 
 module.exports = {
   createProfile,
+  getProfile,
 };
--- src/api/profile/profileRoutes.js
+++ src/api/profile/profileRoutes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const profileController = require('../profile/profileController');
 const { ensuredAuthenticated } = require('../../middleware/authentication');
-// const validator = require('../../middleware/validator');
 const validator = require('../../middleware/validator');
-router.route('/').post(profileController.createProfile);
+router
+  .route('/')
+  .patch(ensuredAuthenticated, profileController.createProfile)
+  .get(ensuredAuthenticated, profileController.getProfile);
 
 module.exports = router;
